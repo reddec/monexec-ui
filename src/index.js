e847ebe0d35f8f028398dba4d34e91bcf50fa3a6
Alexander Baryshnikov <dev@baryshnikov.net>
2018-06-28 21:20:22 +0800

    move loading to component

--- src/index.js
+++ src/index.js
@@ -2,24 +2,5 @@ import Monexec from './components/monexec.html';
 
 let srv = new Monexec({
     target: document.getElementById('supervisors'),
-    data: {
-        configs: fetch('../supervisors', {
-            method: 'get',
-        }).then(function (r) {
-            if (r.status !== 200) {
-                console.warn(r.status, r.statusText)
-            } else {
-                return Promise.all(JSON.parse(r.body).map((name) => {
-                    return fetch('../supervisor/' + encodeURIComponent(name), {
-                        method: 'get',
-                    }).then((data) => {
-                        return JSON.parse(data.body);
-                    })
-                }))
-            }
-
-        }).catch(function (err) {
-            console.error(config.Name, err);
-        })
-    }
+    data: {}
 });
