9e3411c4404f8c75b783709a7057f5d90f09a6ac
Alexander Baryshnikov <dev@baryshnikov.net>
2018-06-28 21:14:43 +0800

    fix urls

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import Monexec from './components/monexec.html';
 let srv = new Monexec({
     target: document.getElementById('supervisors'),
     data: {
-        configs: fetch('supervisors', {
+        configs: fetch('../supervisors', {
             method: 'get',
         }).then(function (r) {
             if (r.status !== 200) {
                 console.warn(r.status, r.statusText)
             } else {
                 return Promise.all(JSON.parse(r.body).map((name) => {
-                    return fetch('supervisor/' + encodeURIComponent(name), {
+                    return fetch('../supervisor/' + encodeURIComponent(name), {
                         method: 'get',
                     }).then((data) => {
                         return JSON.parse(data.body);
